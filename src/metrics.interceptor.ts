132fd9e061ec31240dca494d52cff93f12ea52a9
Ahmad <ahmadasgf89@gmail.com>
2026-03-27 20:40:53 +0700

    chore: update metrics interceptors

--- src/metrics.interceptor.ts
+++ src/metrics.interceptor.ts
@@ -1,25 +1,47 @@
 import {
   CallHandler,
   ExecutionContext,
+  HttpException,
   Injectable,
   NestInterceptor,
 } from '@nestjs/common';
 import { Observable, tap } from 'rxjs';
 import { Request, Response } from 'express';
-
-import {
-  httpRequestCounter,
-  httpErrorCounter,
-  httpDurationHistogram,
-} from './metrics';
+import { MetricService } from 'nestjs-otel';
+import { Counter, Histogram } from '@opentelemetry/api';
 
 @Injectable()
 export class MetricsInterceptor implements NestInterceptor {
+  private httpRequestCounter: Counter;
+  private httpErrorCounter: Counter;
+  private httpDurationHistogram: Histogram;
+
+  constructor(private readonly metricService: MetricService) {
+    this.httpRequestCounter = this.metricService.getCounter(
+      'app_http_requests_total',
+      {
+        description: 'Total HTTP requests',
+      },
+    );
+
+    this.httpErrorCounter = this.metricService.getCounter(
+      'app_http_errors_total',
+      {
+        description: 'Total HTTP errors',
+      },
+    );
+
+    this.httpDurationHistogram = this.metricService.getHistogram(
+      'app_http_duration_ms',
+      {
+        description: 'HTTP request duration',
+      },
+    );
+  }
+
   intercept(context: ExecutionContext, next: CallHandler): Observable<unknown> {
     const now = Date.now();
-
     const http = context.switchToHttp();
-
     const request = http.getRequest<Request>();
     const response = http.getResponse<Response>();
 
@@ -32,30 +54,28 @@ export class MetricsInterceptor implements NestInterceptor {
           const duration = Date.now() - now;
           const status = response.statusCode;
 
-          httpRequestCounter.add(1, {
-            method,
-            route,
-            status,
-          });
-
-          httpDurationHistogram.record(duration, {
+          this.httpRequestCounter.add(1, { method, route, status });
+          this.httpDurationHistogram.record(duration, {
             method,
             route,
             status,
           });
         },
-        error: () => {
+        error: (err: unknown) => {
           const duration = Date.now() - now;
+          let status: number;
 
-          httpErrorCounter.add(1, {
+          if (err instanceof HttpException) {
+            status = err.getStatus();
+          } else {
+            status = 500;
+          }
+
+          this.httpErrorCounter.add(1, { method, route });
+          this.httpDurationHistogram.record(duration, {
             method,
             route,
-          });
-
-          httpDurationHistogram.record(duration, {
-            method,
-            route,
-            status: 500,
+            status,
           });
         },
       }),
